00e70c137a632bbefcd5e3a7d5d498f9c34b1828
shivam-rajyaguru <rajyagurushivam2002@gmail.com>
2024-09-04 00:36:06 +0530

    Setting folder structure and create user schmea

---
+++ e-commerce-backend/dist/app.js
@@ -0,0 +1,6 @@
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`Server is listing on http://localhost : ${port}`);
+});
